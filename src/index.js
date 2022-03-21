1b24319de281838ad31785ec4845232ebefdb0cb
backtofayton <yucelolcay@gmail.com>
2022-03-21 12:51:33 +0200

    Add form

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom"
 
 //component file
 import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(<React.StrictMode><TodoContainer /></React.StrictMode>, document.getElementById("root"))
